37168568fc8cb3fcffcf62987a1f414cbeaa168a
Lalit Kumar <lalit22km@gmail.com>
2023-05-08 00:08:50 +0530

    Used timeapi. Works good enough. Won't be changing.

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,13 +20,16 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
@@ -58,16 +61,35 @@ Partial Class Form1
         '
         Me.Timer1.Interval = 3000
         '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 242)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(352, 96)
+        Me.RichTextBox1.TabIndex = 3
+        Me.RichTextBox1.Text = ""
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(590, 233)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(240, 150)
+        Me.DataGridView1.TabIndex = 4
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(376, 349)
+        Me.ClientSize = New System.Drawing.Size(878, 516)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.RichTextBox1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -77,4 +99,6 @@ Partial Class Form1
     Friend WithEvents Button2 As Button
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Timer1 As Timer
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents DataGridView1 As DataGridView
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,4 +1,10 @@
-﻿Imports Microsoft.Win32
+﻿Imports System.Net
+Imports Microsoft.Win32
+Imports System.Data
+Imports Newtonsoft.Json
+Imports System.IO
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports Newtonsoft.Json.Linq
 
 Public Class Form1
     Dim regKey As RegistryKey
@@ -12,17 +18,25 @@ Public Class Form1
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        ServicePointManager.Expect100Continue = True
+        ServicePointManager.SecurityProtocol = CType(3072, SecurityProtocolType)
+        Dim json As String = (New WebClient).DownloadString("https://timeapi.io/api/Time/current/zone?timeZone=Asia/Kolkata")
+        RichTextBox1.Text = json
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Dim testtime As String = Date.Now.ToString("hhmm")
-        TextBox1.Text = testtime
-        MessageBox.Show(Convert.ToInt32(testtime))
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12
+        Dim json As String = New System.Net.WebClient().DownloadString("https://timeapi.io/api/Time/current/zone?timeZone=Asia/Kolkata")
+        Dim parsejson As JObject = JObject.Parse(json)
+        Dim thehour = parsejson.SelectToken("hour").ToString()
+        Dim themin = parsejson.SelectToken("minute").ToString()
+        TextBox1.Text = ":" + thehour + themin
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         Dim currenttime As String = Date.Now.ToString("hhmm")
 
     End Sub
+
+
 End Class
